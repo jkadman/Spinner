b8f9cc2457b10ff826a4065043ebf4237a39d8bf
Jason Kadman <jkadman231@gmail.com>
2022-11-13 20:37:55 +0000

    made function similar to typewriter to solve this one

---
+++ spinner2.js
@@ -0,0 +1,62 @@
+
+let spinning = '| / - \\ | / - \\ |';
+
+
+let spinner = function (lines, time) {
+  let i = 0;
+  for (let line of lines) {
+    setTimeout(() => {
+      process.stdout.write('\r' + line);
+    }, i += time)
+  }
+  setTimeout(() => {
+    process.stdout.write('\n')
+  }, i += time + 1)
+}
+
+spinner(spinning, 300)
+
+
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 1500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\|   '); 
+// }, 1700);
+
+// setTimeout(() => {
+//   process.stdout.write('\n')
+// }, 1701)
